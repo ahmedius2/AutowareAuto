358763b20b665930a58f5a12e3034c6a2fb7a540
ahmedius2 <ahmedius2@gmail.com>
2024-12-19 21:17:43 +0000

    Point cloud accumulator does not have to wait for ndt matching cuz ekf localization already publishes the transformation with 50 hz

--- src/additional/pc_accumulator_for_dnn/include/pc_accumulator_for_dnn/pc_accumulator_for_dnn_node.hpp
+++ src/additional/pc_accumulator_for_dnn/include/pc_accumulator_for_dnn/pc_accumulator_for_dnn_node.hpp
@@ -30,33 +30,32 @@
 
 namespace pc_acc_for_dnn
 {
-class PCloudAccForDnnComponent : public rclcpp::Node
-{
-public:
-//  PCL_MAKE_ALIGNED_OPERATOR_NEW
-  explicit PCloudAccForDnnComponent(const rclcpp::NodeOptions & options);
+  class PCloudAccForDnnComponent : public rclcpp::Node
+  {
+    public:
+      //  PCL_MAKE_ALIGNED_OPERATOR_NEW
+      explicit PCloudAccForDnnComponent(const rclcpp::NodeOptions & options);
 
-private:
-  double accumulation_time_sec_;
-  int point_cloud_buf_sz_;
-  bool debug_mode_;
+    private:
+      double accumulation_time_sec_;
+      int point_cloud_buf_sz_;
+      bool debug_mode_;
 
-  std::deque<sensor_msgs::msg::PointCloud2::ConstSharedPtr> pcloud_queue_; // each PC2 msg has timestamp in it
-  std::deque<geometry_msgs::msg::TransformStamped::ConstSharedPtr> tf_queue_;
+      std::deque<sensor_msgs::msg::PointCloud2::ConstSharedPtr> pcloud_queue_; // each PC2 msg has timestamp in it
+      std::deque<geometry_msgs::msg::TransformStamped::ConstSharedPtr> tf_queue_;
 
-  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_sub_;
-  rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr egopose_sub_;
-  rclcpp::Publisher<valo_msgs::msg::Float32MultiArrayStamped>::SharedPtr floatarr_pub_;
-  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr debug_pc_pub_;
+      rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_sub_;
+      rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr egopose_sub_;
+      rclcpp::Publisher<valo_msgs::msg::Float32MultiArrayStamped>::SharedPtr floatarr_pub_;
+      rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr debug_pc_pub_;
 
-  std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
-  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+      std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
+      std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
 
-  void pointcloud_callback(const sensor_msgs::msg::PointCloud2::SharedPtr input);
-  void ego_pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr ego_pose);
-};
+      void pointcloud_callback(const sensor_msgs::msg::PointCloud2::SharedPtr input);
+  };
 }  // namespace autoware::pointcloud_preprocessor
 
 // clang-format off
 #endif  // PC_ACC_HPP_  // NOLINT
-// clang-format on
+        // clang-format on
--- src/additional/pc_accumulator_for_dnn/src/pc_accumulator_for_dnn_node.cpp
+++ src/additional/pc_accumulator_for_dnn/src/pc_accumulator_for_dnn_node.cpp
@@ -26,238 +26,223 @@
 
 namespace pc_acc_for_dnn
 {
-PCloudAccForDnnComponent::PCloudAccForDnnComponent(const rclcpp::NodeOptions & options)
-  : Node("PointcloudAccumulatorForDNN", options)
-{
-  accumulation_time_sec_ = declare_parameter<double>("accumulation_time_sec", 0.55);
-  point_cloud_buf_sz_ = declare_parameter<int>("pcloud_queue_size", 10);
-  debug_mode_ = declare_parameter<bool>("debug", false);
+  PCloudAccForDnnComponent::PCloudAccForDnnComponent(const rclcpp::NodeOptions & options)
+    : Node("PointcloudAccumulatorForDNN", options)
+  {
+    accumulation_time_sec_ = declare_parameter<double>("accumulation_time_sec", 0.5);
+    point_cloud_buf_sz_ = declare_parameter<int>("pcloud_queue_size", 10);
+    debug_mode_ = declare_parameter<bool>("debug", false);
 
-  tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
-  tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+    tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+    tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
-  pointcloud_sub_ = create_subscription<sensor_msgs::msg::PointCloud2>(
-   "input/pointcloud", rclcpp::SensorDataQoS(),
-    std::bind(&PCloudAccForDnnComponent::pointcloud_callback, this, std::placeholders::_1));
+    pointcloud_sub_ = create_subscription<sensor_msgs::msg::PointCloud2>(
+        "input/pointcloud", rclcpp::SensorDataQoS(),
+        std::bind(&PCloudAccForDnnComponent::pointcloud_callback, this, std::placeholders::_1));
 
-  egopose_sub_ = create_subscription<geometry_msgs::msg::PoseStamped>(
-    //"/localization/pose_estimator/pose", 10,
-    "/localization/pose_estimator/pose", 10,
-    std::bind(&PCloudAccForDnnComponent::ego_pose_callback, this, std::placeholders::_1));
-
-  floatarr_pub_ = create_publisher<valo_msgs::msg::Float32MultiArrayStamped>(
-    "accumulated_pc_as_arr", 10);
-
-  if(debug_mode_){
-    debug_pc_pub_ = create_publisher<sensor_msgs::msg::PointCloud2>(
-        "debug/accumulated_pointcloud", 10);
-  }
-}
-
-void PCloudAccForDnnComponent::pointcloud_callback(
-  const sensor_msgs::msg::PointCloud2::SharedPtr input) 
-{
-  pcloud_queue_.push_back(input);
-
-  // Remove point clouds that is beyond time lim
-  rclcpp::Time most_recent_pc_time = input->header.stamp;
-  double tdiff = (most_recent_pc_time - pcloud_queue_.front()->header.stamp).seconds();
-  while(pcloud_queue_.size() > 1 && tdiff > accumulation_time_sec_){
-    pcloud_queue_.pop_front();
-    tdiff = (most_recent_pc_time - pcloud_queue_.front()->header.stamp).seconds();
-  }
-
-  while(pcloud_queue_.size() > point_cloud_buf_sz_){
-    pcloud_queue_.pop_front();
-  }
-}
-
-
-void PCloudAccForDnnComponent::ego_pose_callback(
-  const geometry_msgs::msg::PoseStamped::SharedPtr ego_pose) 
-{
-	//Since ego_pose came, the corresponding tf should be available
-	geometry_msgs::msg::TransformStamped::ConstSharedPtr tf;
-
-	auto start = std::chrono::steady_clock::now();
-  //NOTE this can take a long time, gotta check
-	try {
-		auto tf_res = tf_buffer_->lookupTransform("base_link", "map", tf2::TimePointZero);
-    rclcpp::Time t1 = ego_pose->header.stamp, t2 = tf_res.header.stamp;
-    float tdiff = (t2-t1).seconds();
-    if(tdiff*1000 > 95){
-      RCLCPP_WARN(this->get_logger(), "point cloud and tf tdiff is greater than 95 ms: %.1f ms",
-          tdiff * 1000);
-    }
+    floatarr_pub_ = create_publisher<valo_msgs::msg::Float32MultiArrayStamped>(
+        "accumulated_pc_as_arr", 10);
 
     if(debug_mode_){
-  		RCLCPP_INFO(this->get_logger(), "tdiff: %.1f ms", tdiff * 1000);
+      debug_pc_pub_ = create_publisher<sensor_msgs::msg::PointCloud2>(
+          "debug/accumulated_pointcloud", 10);
     }
-		tf_res.header.stamp = ego_pose->header.stamp;
-		tf = std::make_shared<geometry_msgs::msg::TransformStamped>(tf_res);
-  	tf_queue_.push_back(tf);
-	} catch (const tf2::TransformException & ex) {
-		RCLCPP_ERROR(
-			this->get_logger(), "Could not transform %s to %s: %s",
-			"base_link", "map", ex.what());
-	}
-
-  if(pcloud_queue_.empty())
-    return;
-
-  auto most_recent_pc = pcloud_queue_.back();
-  rclcpp::Time most_recent_pc_time = most_recent_pc->header.stamp;
-  double tdiff = (most_recent_pc_time - tf_queue_.front()->header.stamp).seconds();
-  while(tf_queue_.size() > 1 && tdiff > accumulation_time_sec_){
-    tf_queue_.pop_front();
-    tdiff = (most_recent_pc_time - tf_queue_.front()->header.stamp).seconds();
   }
 
-  // Find the point cloud of each egopose
-  // the head of the queue has the most recent paired cloud
-  std::deque<std::pair<sensor_msgs::msg::PointCloud2::ConstSharedPtr, 
-    geometry_msgs::msg::TransformStamped::ConstSharedPtr>> pairs;
-  unsigned num_all_points = 0;
-  for(auto pc : pcloud_queue_){
-    rclcpp::Time pc_ts = pc->header.stamp;
-    for(auto tf_sel : tf_queue_){
-      rclcpp::Time tf_ts = tf_sel->header.stamp;
-      if(pc_ts == tf_ts){ // it ok to compare like this
-        pairs.push_front(std::make_pair(pc, tf_sel));
-        num_all_points += pc->width;
-        break;
+  void PCloudAccForDnnComponent::pointcloud_callback(
+      const sensor_msgs::msg::PointCloud2::SharedPtr input)
+  {
+    auto start = std::chrono::steady_clock::now();
+    pcloud_queue_.push_back(input);
+
+    // Remove point clouds that is beyond time lim
+    rclcpp::Time most_recent_pc_time = input->header.stamp;
+    double tdiff = (most_recent_pc_time - pcloud_queue_.front()->header.stamp).seconds();
+    while(pcloud_queue_.size() > 1 && tdiff > accumulation_time_sec_){
+      pcloud_queue_.pop_front();
+      tdiff = (most_recent_pc_time - pcloud_queue_.front()->header.stamp).seconds();
+    }
+
+    while(pcloud_queue_.size() > point_cloud_buf_sz_){
+      pcloud_queue_.pop_front();
+    }
+
+    geometry_msgs::msg::TransformStamped::ConstSharedPtr tf;
+    rclcpp::Time tf_time;
+
+    try {
+      rclcpp::Duration dur(0, 100000000); // Timout 100 milliseconds, but it should just take 1 ms
+      auto tf_res = tf_buffer_->lookupTransform("base_link", "map", most_recent_pc_time, dur);
+      tf_time = tf_res.header.stamp;
+      tdiff = std::abs((most_recent_pc_time - tf_time).seconds());
+      if(tdiff*1000 > 20 || debug_mode_){
+        RCLCPP_INFO(this->get_logger(), "point cloud and tf tdiff: %.1f ms", tdiff * 1000);
+      }
+
+      tf_res.header.stamp = input->header.stamp; // Just to make sure they are same
+      tf = std::make_shared<geometry_msgs::msg::TransformStamped>(tf_res);
+      tf_queue_.push_back(tf);
+    }
+    catch (const tf2::TransformException & ex) {
+      RCLCPP_ERROR(
+          this->get_logger(), "Could not transform %s to %s: %s",
+          "base_link", "map", ex.what());
+    }
+
+    if(pcloud_queue_.empty() || tf_queue_.empty())
+      return;
+
+    tdiff = (most_recent_pc_time - tf_queue_.front()->header.stamp).seconds();
+    while(tf_queue_.size() > 1 && tdiff > accumulation_time_sec_){
+      tf_queue_.pop_front();
+      tdiff = (most_recent_pc_time - tf_queue_.front()->header.stamp).seconds();
+    }
+
+    // Find the point cloud of each egopose
+    // the head of the queue has the most recent paired cloud
+    std::deque<std::pair<sensor_msgs::msg::PointCloud2::ConstSharedPtr,
+      geometry_msgs::msg::TransformStamped::ConstSharedPtr>> pairs;
+    unsigned num_all_points = 0;
+    for(auto pc : pcloud_queue_){
+      rclcpp::Time pc_ts = pc->header.stamp;
+      for(auto tf_sel : tf_queue_){
+        rclcpp::Time tf_ts = tf_sel->header.stamp;
+        if(pc_ts == tf_ts){ // it ok to compare like this
+          pairs.push_front(std::make_pair(pc, tf_sel));
+          num_all_points += pc->width;
+          break;
+        }
       }
     }
-  }
 
-  if(pairs.empty() || (pairs.front().first->header.stamp != most_recent_pc_time)){
-    // Publish the most recent point cloud only
-    num_all_points = most_recent_pc->width;
-    pairs.clear();
-    pairs.push_front(std::make_pair(most_recent_pc, nullptr));
-  }
+    if(pairs.empty() || (pairs.front().first->header.stamp != most_recent_pc_time)){
+      // Publish the most recent point cloud only
+      num_all_points = input->width;
+      pairs.clear();
+      pairs.push_front(std::make_pair(input, nullptr));
+    }
 
-  valo_msgs::msg::Float32MultiArrayStamped multarr;
-  multarr.header.frame_id = "velodyne_top";
-  multarr.header.stamp = pairs.front().first->header.stamp;
-  auto num_fields = 4; // x, y, z, t
-  auto dim1 = std_msgs::msg::MultiArrayDimension();
-  dim1.label = "num_points";
-  dim1.size = num_all_points;
-  dim1.stride = num_all_points * num_fields;
-  auto dim2 = std_msgs::msg::MultiArrayDimension();
-  dim2.label = "xyzt"; // no intensity cuz awsim doesnt have it
-  dim2.size = num_fields;
-  dim2.stride = num_fields;
-  multarr.array.layout.dim.push_back(dim1);
-  multarr.array.layout.dim.push_back(dim2);
-  multarr.array.layout.data_offset = 0;
-  multarr.array.data.resize(num_all_points * num_fields);
+    valo_msgs::msg::Float32MultiArrayStamped multarr;
+    multarr.header.frame_id = "velodyne_top";
+    multarr.header.stamp = pairs.front().first->header.stamp;
+    auto num_fields = 4; // x, y, z, t
+    auto dim1 = std_msgs::msg::MultiArrayDimension();
+    dim1.label = "num_points";
+    dim1.size = num_all_points;
+    dim1.stride = num_all_points * num_fields;
+    auto dim2 = std_msgs::msg::MultiArrayDimension();
+    dim2.label = "xyzt"; // no intensity cuz awsim doesnt have it
+    dim2.size = num_fields;
+    dim2.stride = num_fields;
+    multarr.array.layout.dim.push_back(dim1);
+    multarr.array.layout.dim.push_back(dim2);
+    multarr.array.layout.data_offset = 0;
+    multarr.array.data.resize(num_all_points * num_fields);
 
-  sensor_msgs::msg::PointCloud2 debug_pc;
-  if(debug_mode_){
-    debug_pc = *most_recent_pc;
-    debug_pc.header.frame_id = "velodyne_top";
-    debug_pc.width = num_all_points;
-    debug_pc.data.resize(num_all_points * debug_pc.point_step); // bytes array
-  }
+    sensor_msgs::msg::PointCloud2 debug_pc;
+    if(debug_mode_){
+      debug_pc = *input;
+      debug_pc.header.frame_id = "velodyne_top";
+      debug_pc.width = num_all_points;
+      debug_pc.data.resize(num_all_points * debug_pc.point_step); // bytes array
+    }
 
-  Eigen::Quaternion<float> q1(
-      tf->transform.rotation.w,
-      tf->transform.rotation.x,
-      tf->transform.rotation.y,
-      tf->transform.rotation.z);
-  Eigen::Transform<float,3,Eigen::Affine> map_to_bl_t = Eigen::Translation3f(
-      tf->transform.translation.x,
-      tf->transform.translation.y,
-      tf->transform.translation.z) * q1;
+    Eigen::Quaternion<float> q1(
+        tf->transform.rotation.w,
+        tf->transform.rotation.x,
+        tf->transform.rotation.y,
+        tf->transform.rotation.z);
+    Eigen::Transform<float,3,Eigen::Affine> map_to_bl_t = Eigen::Translation3f(
+        tf->transform.translation.x,
+        tf->transform.translation.y,
+        tf->transform.translation.z) * q1;
 
-  // This is a static transformation
-  auto baselink_to_velotop_t = tf_buffer_->lookupTransform("velodyne_top",
-      "base_link", tf2::TimePointZero);
+    // This is a static transformation
+    auto baselink_to_velotop_t = tf_buffer_->lookupTransform("velodyne_top",
+        "base_link", tf2::TimePointZero);
 
-  Eigen::Quaternion<float> q3(
-      baselink_to_velotop_t.transform.rotation.w,
-      baselink_to_velotop_t.transform.rotation.x,
-      baselink_to_velotop_t.transform.rotation.y,
-      baselink_to_velotop_t.transform.rotation.z);
-  Eigen::Transform<float,3,Eigen::Affine> bl_to_vlt_t = Eigen::Translation3f(
-      baselink_to_velotop_t.transform.translation.x,
-      baselink_to_velotop_t.transform.translation.y,
-      baselink_to_velotop_t.transform.translation.z) * q3;
+    Eigen::Quaternion<float> q3(
+        baselink_to_velotop_t.transform.rotation.w,
+        baselink_to_velotop_t.transform.rotation.x,
+        baselink_to_velotop_t.transform.rotation.y,
+        baselink_to_velotop_t.transform.rotation.z);
+    Eigen::Transform<float,3,Eigen::Affine> bl_to_vlt_t = Eigen::Translation3f(
+        baselink_to_velotop_t.transform.translation.x,
+        baselink_to_velotop_t.transform.translation.y,
+        baselink_to_velotop_t.transform.translation.z) * q3;
 
-  int point_counter = 0;
-  for (auto p : pairs){ // from newest to oldest
-    auto pc_msg = *(p.first);
-    auto tf_sel = p.second; // this can be nullptr!
+    int point_counter = 0;
+    for (auto p : pairs){ // from newest to oldest
+      auto pc_msg = *(p.first);
+      auto tf_sel = p.second; // this can be nullptr!
 
-    float tdiff = (most_recent_pc_time - pc_msg.header.stamp).seconds();
-    sensor_msgs::PointCloud2ConstIterator<float> 
-      x_itr(pc_msg, "x"), y_itr(pc_msg, "y"), z_itr(pc_msg, "z");
-    sensor_msgs::PointCloud2ConstIterator<uint8_t> 
-      i_itr(pc_msg, "intensity"), rt_itr(pc_msg, "return_type");
-    sensor_msgs::PointCloud2ConstIterator<uint16_t> c_itr(pc_msg, "channel"); 
-  
-    Eigen::MatrixXf points(4, pc_msg.width);
-    for (size_t i = 0; x_itr != x_itr.end(); ++x_itr, ++y_itr, ++z_itr, ++i) {
+      float tdiff = (most_recent_pc_time - pc_msg.header.stamp).seconds();
+      sensor_msgs::PointCloud2ConstIterator<float>
+        x_itr(pc_msg, "x"), y_itr(pc_msg, "y"), z_itr(pc_msg, "z");
+      sensor_msgs::PointCloud2ConstIterator<uint8_t>
+        i_itr(pc_msg, "intensity"), rt_itr(pc_msg, "return_type");
+      sensor_msgs::PointCloud2ConstIterator<uint16_t> c_itr(pc_msg, "channel");
+
+      Eigen::MatrixXf points(4, pc_msg.width);
+      for (size_t i = 0; x_itr != x_itr.end(); ++x_itr, ++y_itr, ++z_itr, ++i) {
         points(0, i) = *x_itr;
         points(1, i) = *y_itr;
         points(2, i) = *z_itr;
-    }
-    points.row(3).setOnes();
+      }
+      points.row(3).setOnes();
 
-    if(tdiff != .0){
-      Eigen::Quaternion<float> q2(
-          tf_sel->transform.rotation.w,
-          tf_sel->transform.rotation.x,
-          tf_sel->transform.rotation.y,
-          tf_sel->transform.rotation.z);
-      Eigen::Transform<float,3,Eigen::Affine> map_to_bl_t2= Eigen::Translation3f(
-          tf_sel->transform.translation.x,
-          tf_sel->transform.translation.y,
-          tf_sel->transform.translation.z) * q2;
+      if(tdiff != .0){
+        Eigen::Quaternion<float> q2(
+            tf_sel->transform.rotation.w,
+            tf_sel->transform.rotation.x,
+            tf_sel->transform.rotation.y,
+            tf_sel->transform.rotation.z);
+        Eigen::Transform<float,3,Eigen::Affine> map_to_bl_t2= Eigen::Translation3f(
+            tf_sel->transform.translation.x,
+            tf_sel->transform.translation.y,
+            tf_sel->transform.translation.z) * q2;
 
-      points = bl_to_vlt_t.matrix() * map_to_bl_t.matrix() * \
-               map_to_bl_t2.inverse().matrix() * points;
-    }
-    else{
-      points = bl_to_vlt_t.matrix() * points;
-    }
+        points = bl_to_vlt_t.matrix() * map_to_bl_t.matrix() * \
+                 map_to_bl_t2.inverse().matrix() * points;
+      }
+      else{
+        points = bl_to_vlt_t.matrix() * points;
+      }
 
-    for(size_t i=0; i_itr != i_itr.end(); ++i, ++i_itr, ++point_counter){
-      //NOTE still need to do the transformation and timing
-      auto idx = point_counter * num_fields;
-      //multarr.array.data[idx] = 0; // batch size always 0
-      multarr.array.data[idx] = points(0, i);
-      multarr.array.data[idx+1] = points(1, i);
-      multarr.array.data[idx+2] = points(2, i);
-      multarr.array.data[idx+3] = tdiff; // save it as sec
-      if(debug_mode_){
-        // ignore rettype and channel?
-        uint8_t* addr = &debug_pc.data[point_counter * debug_pc.point_step];
-        ((float*)addr)[0] = points(0, i);
-        ((float*)addr)[1] = points(1, i);
-        ((float*)addr)[2] = points(2, i);
-        addr += sizeof(float)*3;
-        addr[0] = *i_itr; // its just 0 for awsim but save it anyway
-        addr[1] = *rt_itr;
-        addr += sizeof(uint8_t)*2;
-        ((uint16_t*)addr)[0] = *c_itr;
-        ++rt_itr; ++c_itr;
+      for(size_t i=0; i_itr != i_itr.end(); ++i, ++i_itr, ++point_counter){
+        //NOTE still need to do the transformation and timing
+        auto idx = point_counter * num_fields;
+        //multarr.array.data[idx] = 0; // batch size always 0
+        multarr.array.data[idx] = points(0, i);
+        multarr.array.data[idx+1] = points(1, i);
+        multarr.array.data[idx+2] = points(2, i);
+        multarr.array.data[idx+3] = tdiff; // save it as sec
+        if(debug_mode_){
+          // ignore rettype and channel?
+          uint8_t* addr = &debug_pc.data[point_counter * debug_pc.point_step];
+          ((float*)addr)[0] = points(0, i);
+          ((float*)addr)[1] = points(1, i);
+          ((float*)addr)[2] = points(2, i);
+          addr += sizeof(float)*3;
+          addr[0] = *i_itr; // its just 0 for awsim but save it anyway
+          addr[1] = *rt_itr;
+          addr += sizeof(uint8_t)*2;
+          ((uint16_t*)addr)[0] = *c_itr;
+          ++rt_itr; ++c_itr;
+        }
       }
     }
-  }
 
-  floatarr_pub_->publish(multarr);
+    floatarr_pub_->publish(multarr);
 
-  auto end = std::chrono::steady_clock::now();
-  std::chrono::duration<double, std::milli> duration = end - start;
-  if(debug_mode_){
-    debug_pc_pub_->publish(debug_pc);
-    RCLCPP_INFO(this->get_logger(), "Published %d points from %d point clouds. Took %.1f ms.",
-        point_counter, pairs.size(), duration.count());
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double, std::milli> duration = end - start;
+    if(debug_mode_){
+      debug_pc_pub_->publish(debug_pc);
+      RCLCPP_INFO(this->get_logger(), "Published %d points from %d point clouds. Took %.1f ms.",
+          point_counter, pairs.size(), duration.count());
+    }
   }
-}
 }  // namespace pc_acc_for_dnn
 
 #include <rclcpp_components/register_node_macro.hpp>
